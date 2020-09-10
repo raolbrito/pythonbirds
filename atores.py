c8041af1f7d332657ace97939a39b264927688d1
rbrito <ra.brito@yahoo.com.br>
2020-09-10 15:37:59 -0300

    add lançamento vertical

--- atores.py
+++ atores.py
@@ -55,7 +55,7 @@ class Ator():
         if self.status == ATIVO and outro_ator.status == ATIVO:
             delta_x = abs(self.x - outro_ator.x)
             delta_y = abs(self.y - outro_ator.y)
-            if delta_x<=1 and delta_y<=1:
+            if delta_x<=intervalo and delta_y<=intervalo:
                 self.status=outro_ator.status=DESTRUIDO
 
 
@@ -68,6 +68,7 @@ class Obstaculo(Ator):
 
 class Porco(Ator):
     _caracter_ativo = '@'
+    _caracter_destruido = '+'
 
 
 class DuploLancamentoExcecao(Exception):
@@ -99,7 +100,7 @@ class Passaro(Ator):
 
         :return: booleano
         """
-        return True
+        return not self._tempo_de_lancamento is None
 
     def colidir_com_chao(self):
         """
@@ -123,7 +124,11 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1, 1
+        if self.foi_lancado():
+            delta_t= tempo-self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+        return super().calcular_posicao(tempo)
+
 
 
     def lancar(self, angulo, tempo_de_lancamento):
@@ -135,12 +140,25 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        pass
+        self._angulo_de_lancamento = angulo
+        self._tempo_de_lancamento = tempo_de_lancamento
+
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual = self._y_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
+        self.y = y_atual
+
 
 
 class PassaroAmarelo(Passaro):
     _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
     _caracter_ativo = 'V'
+    _caracter_destruido = 'v'
+    velocidade_escalar = 20
